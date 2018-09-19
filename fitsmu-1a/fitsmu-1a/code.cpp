844052a4018b2c666f3a85d03b1ba562cba93cc9
Sam Smucny <smucny.sam@gmail.com>
2018-09-18 22:31:20 -0400

    Fefactored code and comments. Added more consts and moved some functions into private. Verified correctness of algorithm.

--- fitsmu-1a/fitsmu-1a/code.cpp
+++ fitsmu-1a/fitsmu-1a/code.cpp
@@ -9,12 +9,11 @@ code::code()
 }
 
 // Instantiating Constructor, calls generateSecretCode method to
-//  fill the secret vector with random integers
+// fill the secret vector with random integers
 //
 // codeLen: Defines the length of the secret code
 // maxDig: Defines the maximum possible digit in the code
-code::code(int codeLen,
-           int maxDig)
+code::code(const int& codeLen, const int& maxDig)
 {
 	this->codeLen = codeLen;
 	this->maxDig = maxDig;
@@ -25,8 +24,7 @@ code::code(int codeLen,
 //
 // newSecret: Vector containing the integers of a secret code
 // maxDigit: Defines the maximum possible digit in the code
-code::code(const std::vector<int>& newSecret,
-           const int& maxDigit)
+code::code(const std::vector<int>& newSecret, const int& maxDigit)
 {
 	secret = newSecret;
 	codeLen = newSecret.size();
@@ -35,13 +33,12 @@ code::code(const std::vector<int>& newSecret,
 
 // Check if the passed integer is contained in the passed vector
 // This is a static method because has no instance importance but is
-//  related to code class
+// related to code class
 //
 // v: Vector of integers populated by function, if "this" object
 //    contains the digit
 // digit: Vector is checked to see if it contains this int
-bool code::find(const std::vector<int>& v,
-                const int digit)
+bool code::find(const std::vector<int>& v, const int& digit)
 {
 	for (int i = 0; i < v.size(); i++)
 	{
@@ -53,28 +50,25 @@ bool code::find(const std::vector<int>& v,
 }
 
 // Check if the correct digits in the correct place is equal to the length
-//  of the secret code. Then the code has been guessed
+// of the secret code. Then the code has been guessed
 //
 // codeGuessOutput: the structure containing the output of checking the
 //                  user code against the secret code. Passed by reference
 //                  so the struct in the main funtion can be changed
 bool code::checkWin(const codeGuess& codeGuessOutput) const
 {
-	if (codeGuessOutput.correctDigitCorrectPlacement == this->codeLen)
-		return true;
-	else
-		return false;
+	return codeGuessOutput.correctDigitCorrectPlacement == this->codeLen;
 }
 
 // Checks how the code that the user has input matches with the
-//  secret code. This will return a struct that contains 2 integers
-//  that tell how many correct digits were in the correst place and
-//  how many correct digits were in the incorrect place.
+// secret code. This will return a struct that contains 2 integers
+// that tell how many correct digits were in the correst place and
+// how many correct digits were in the incorrect place.
 // To get the value of the integers, two helper functions are called
-//  in each case to compare the two code objects
+// in each case to compare the two code objects
 //
-//  userCode: The code object containing the secret code guessed by
-//            the player
+// userCode: The code object containing the code guessed by
+//           the player
 codeGuess code::checkUserInput(const code& userCode) const
 {
 	codeGuess codeGuessOutput;
@@ -82,12 +76,11 @@ codeGuess code::checkUserInput(const code& userCode) const
 	codeGuessOutput.correctDigitIncorrectPlacement = checkIncorrect(userCode);
 
 	return codeGuessOutput;
-
 }
 
 // Check if the User input code is of correct parameters: i.e.
-//  the maximum user input digit is <= the maximum digit defined
-//  in the secret code
+// the maximum user input digit is <= the maximum digit defined
+// in the secret code
 bool code::checkValidity() const
 {
 	for (int i = 0; i < codeLen; i++)
@@ -100,8 +93,8 @@ bool code::checkValidity() const
 }
 
 // Creates the vector containing the user code by asking for
-//  user input
-code code::generateUserCode()
+// user input
+code code::generateUserCode() const
 {
 	code userCode(this->codeLen, this->maxDig);
 	userCode.secret.resize(this->codeLen);
@@ -133,59 +126,59 @@ int code::checkCorrect(const code & guess) const
 }
 
 // Check how many digits in the user code are in the correct place in
-//  the secret code
+// the secret code
 // Adds the index of these correctly placed digits into the checkedDigits vector
-//  so the code::checkIncorrect method does not mistakenly take a correctly placed
-//  digit for an incorrect one (i.e. if the digit was correctly placed at index 4,
-//  but another instace of the digit is as index 1)
+// so the code::checkIncorrect method does not mistakenly take a correctly placed
+// digit for an incorrect one (i.e. if the digit was correctly placed at index 4,
+// but another instace of the digit is as index 1)
 //
-//  checkedDigits: The vector containing the indices of the number in the secret
-//                 code that have already been mathched to the user's guess
-//  guess: The code object containing the secret code guessed by
-//         the player
-void code::checkCorrectForIncorrect(std::vector<int>& checkedDigits,
-                                    const code& guess) const{
-  for (int i = 0; i < this->codeLen; i++)
-  {
-    if (guess.secret.at(i) == this->secret.at(i))
-      checkedDigits.push_back(i);
-  }
+// checkedDigits: The vector containing the indices of the number in the secret
+//                code that have already been mathched to the user's guess
+// guess: The code object containing the secret code guessed by
+//        the player
+void code::checkCorrectForIncorrect(std::vector<int>& checkedDigits, const code& guess) const
+{
+	for (int i = 0; i < this->codeLen; i++)
+	{
+		if (guess.secret.at(i) == this->secret.at(i))
+			checkedDigits.push_back(i);
+	}
 }
 
 // Returns the number of correct digits placed incorrectly. In other words, how
-//  how many digits are in the secret code, but have been entered at the incorrect
-//  index.
+// how many digits are in the secret code, but have been entered at the incorrect
+// index.
 // The method only counts instances once, using a local vector to keep track
-//  of indexes already verified to have the correct digit.
+// of indexes already verified to have the correct digit.
 //
-//  guess: The code object containing the secret code guessed by
-//         the player
+// guess: The code object containing the secret code guessed by
+//        the player
 int code::checkIncorrect(const code& guess) const
 {
 	// Array containing digits already checked in the secret code
 	std::vector<int> checkedDigitsSecret;
-  std::vector<int> checkedDigitsUser;
-  int correctDigitIncorrectPosition = 0;
+	std::vector<int> checkedDigitsUser;
+	int correctDigitIncorrectPosition = 0;
 
-  // Called to preallocate all indices that have correct guesses in
-  //  correct positions
-  this->checkCorrectForIncorrect(checkedDigitsSecret, guess);
-  this->checkCorrectForIncorrect(checkedDigitsUser, guess);
+	// Called to preallocate all indices that have correct guesses in
+	// correct positions
+	this->checkCorrectForIncorrect(checkedDigitsSecret, guess);
+	this->checkCorrectForIncorrect(checkedDigitsUser, guess);
 
-  // Loop to check through both the user guess and the secret code and
-  //  compare their digits
+	// Loop to check through both the user guess and the secret code and
+	// compare their digits
 	for (int i = 0; i < guess.codeLen; i++)
 	{
 		for (int j = 0; j < this->codeLen; j++)
 		{
 			if (j != i &&
-          !code::find(checkedDigitsSecret, j) &&
-          !code::find(checkedDigitsUser, i) &&
-          (this->secret.at(j) == guess.secret.at(i)))
+				!code::find(checkedDigitsSecret, j) &&
+				!code::find(checkedDigitsUser, i) &&
+				(this->secret.at(j) == guess.secret.at(i)))
 			{
 				correctDigitIncorrectPosition++;
 				checkedDigitsSecret.push_back(j);
-        checkedDigitsUser.push_back(i);
+				checkedDigitsUser.push_back(i);
 				break; // necessary to not count multiple instances of digit placed incorrectly
 			}
 		}
@@ -203,8 +196,8 @@ void code::generateSecretCode()
 
 	for (int i = 0; i < codeLen; i++)
 	{
-		// generate random number from 1 to maxDig
-		randomNumber = rand() % (maxDig+1);
+		// generate random number from 0 to maxDig
+		randomNumber = rand() % (maxDig + 1);
 		secret.push_back(randomNumber);
 	}
 }
--- fitsmu-1a/fitsmu-1a/code.h
+++ fitsmu-1a/fitsmu-1a/code.h
@@ -5,7 +5,7 @@
 #include <time.h>
 
 // Structure defining the output of checking the user code input
-//  compared to the secret code
+// compared to the secret code
 // correctDigitCorrectPlacement: how many digits were guessed in the
 //                                correct positions
 // correctDigitIncorrectPlacement: how many digits were guessed
@@ -18,36 +18,36 @@ struct codeGuess
 
 // Main class defining a code, secret or user input
 // A code is defined by its length and the value of
-//  the maximum digit, as well as a vector defining the code
+// the maximum digit, as well as a vector defining the code
 class code
 {
 public:
 
-  // Constructors
+	// Constructors
 	code();
-	code(int codeLen, int maxDig);
+	code(const int& codeLen, const int& maxDig);
 	code(const std::vector<int>& secret, const int& maxDigit);
 
-  // Non-Static Member Functions
+	// Non-Static Member Functions
 	void printCode() const;
-	code generateUserCode();
+	code generateUserCode() const;
 	bool checkValidity() const;
 	int checkCorrect(const code& guess) const;
 	int checkIncorrect(const code& guess) const;
 	codeGuess checkUserInput(const code& userCode) const;
-  void checkCorrectForIncorrect(std::vector<int>& checkedDigits, const code& guess) const;
 	bool checkWin(const codeGuess& codeGuessOutput) const;
-
-  // Static Member Functions
-	static bool find(const std::vector<int>& v, const int digit);
 
 private:
 
-  // Fields
+	// Fields
 	int codeLen;
 	int maxDig;
 	std::vector<int> secret;
 
-  // Private Method
+	// Private Method
 	void generateSecretCode();
+	void checkCorrectForIncorrect(std::vector<int>& checkedDigits, const code& guess) const;
+
+	// Static Member Functions
+	static bool find(const std::vector<int>& v, const int& digit);
 };
